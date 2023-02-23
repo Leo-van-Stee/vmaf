7a4732714d0220b6b69c628aa40d0c9a9f327538
Leo-van-Stee <66698300+Leo-van-Stee@users.noreply.github.com>
2023-02-23 14:07:47 +0100

    Update output.c

--- libvmaf/src/output.c
+++ libvmaf/src/output.c
@@ -318,6 +318,9 @@ int output_get_outputline_sub_Leo(VmafFeatureCollector* fc, unsigned frame, char
             continue;
         if (!fc->feature_vector[featidx]->score[frame].written)
             continue;
+        if (strpos == 0)
+            strpos += snprintf(outputline + strpos, 510, "%05d ", frame);
+
         strpos += snprintf(outputline + strpos, 510, "%s: %.6f|",
             vmaf_feature_name_alias(fc->feature_vector[featidx]->name),
             fc->feature_vector[featidx]->score[frame].value);
